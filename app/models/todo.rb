cfc7b1cb8ff3fdbf32a62f40ec69081434a20cf1
rahul <rahuks123@gmail.com>
2020-03-27 20:33:56 +0530

    shows record based on id

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,6 @@
-class Todo<ActiveRecord::Base
-    def to_pleasant_string
-        is_completed = completed ? "[x]" : "[]"
-        "#{id} #{is_completed} #{todo_text} #{due_date}"
-    end
+class Todo < ActiveRecord::Base
+  def to_pleasant_string
+    is_completed = completed ? "[x]" : "[]"
+    "#{id} #{is_completed} #{todo_text} #{due_date}"
+  end
 end
