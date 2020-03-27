a1a51adb440760d92379d696803587b1fdd75a10
rahul <rahuks123@gmail.com>
2020-03-27 20:37:23 +0530

    create a new record

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -8,4 +8,15 @@ class TodosController < ApplicationController
     todo = Todo.find(id)
     render plain: todo.to_pleasant_string
   end
+
+  def create
+    todo_text = params[:todo_text]
+    due_date = DateTime.parse(params[:due_date])
+    new_todo = Todo.create!(
+      todo_text: todo_text,
+      due_date: due_date,
+      completed: false,
+    )
+    render plain: "the new record is created with the id #{new_todo.id}"
+  end
 end
