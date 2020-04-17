af3ed4483a2c2279666427aaf579e7971cc9c954
rahul <rahuks123@gmail.com>
2020-04-17 20:18:33 +0530

    Updated database migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_28_051136) do
+ActiveRecord::Schema.define(version: 2020_04_17_143319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "todos", force: :cascade do |t|
+    t.text "todo_text"
     t.date "due_date"
-    t.string "todo_text"
     t.boolean "completed"
   end
 
