54c0e7bc84af41d740b88effc7f7d4ed68665e81
rahul <rahuks123@gmail.com>
2020-04-18 21:05:22 +0530

    now we can update todos using checkbox

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -29,6 +29,6 @@ class TodosController < ApplicationController
     todo = Todo.find(id)
     todo.completed = completed
     todo.save!
-    render plain: "the change has been updated"
+    redirect_to todos_path
   end
 end
