b6c4d3e460afafdc69b2831c9eeb75f1ef51c4e7
rahul <rahuks123@gmail.com>
2020-04-18 19:06:18 +0530

    added submit functionality to add button

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -5,7 +5,7 @@ class Todo < ActiveRecord::Base
   end
 
   def self.overdue
-    all.where("due_date < ? and (not completed)", Date.today)   #instead of checking each row we are checking all the row of the table
+    all.where("due_date < ? and (not completed)", Date.today)
   end
 
   def self.due_today
