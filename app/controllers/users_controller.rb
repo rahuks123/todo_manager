dbd24315311c1a512eaaed3894416bb46caaab78
rahul <rahuks123@gmail.com>
2020-04-21 01:11:24 +0530

    now new users can sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,14 @@
 class UsersController < ApplicationController
-  skip_before_action :verify_authenticity_token
-
-  def index
-    render plain: User.all.map { |user| user.display }.join("\n")
+  def new
+    render "users/new"
   end
 
   def create
-    email = params[:email]
-    password = params[:password]
-    name = params[:name]
-    new_user = User.create!(
-      email: email,
-      password: password,
-      name: name,
+    User.create!(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
     )
-    render plain: "the new record is created with id #{new_user.id} "
-  end
-
-  def show
-    id = params[:id]
-    user = User.find(id)
-    render plain: user.display
-  end
-
-  def login
-    email = params[:email]
-    password = params[:password]
-    user = User.find_by "email = ? and password = ?", email, password
-    if (user)
-      render plain: "true"
-    else
-      render plain: "false"
-    end
+    redirect_to "/"
   end
 end
