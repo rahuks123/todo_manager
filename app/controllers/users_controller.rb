2492e6441eaa549954213d31980dd5c9aa314ab5
rahul <rahuks123@gmail.com>
2020-04-21 12:29:16 +0530

    has_secure_password for security of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
+      password: params[:password],
     )
     redirect_to "/"
   end
